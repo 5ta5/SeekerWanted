40a5a233e0369ddf1edb50d43d8e9b2d1280eee1
user_removed <s4me@int.pl>
2021-05-30 20:54:09 +0200

    orto

--- Source/SeekerWanted/public/Seeker.h
+++ Source/SeekerWanted/public/Seeker.h
@@ -23,7 +23,7 @@ public:
 	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
 
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="Detection");
-	float DetectionDistance;//maximum distance to wanted actor that can be get spotted
+	float DetectionDistance;//maximum distance to wanted actor that can get spotted
 
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Debug");
 	bool ScreenDebug;//Onscreen debug messages and Lines Drawn between objects
@@ -33,15 +33,15 @@ private:
 	TArray<AActor*> UnDetected;
 	void RefreshTargets();//Find all wanted actors in the world
 	static void SortTargets(AActor* P, TArray<AActor*> in, TArray<AActor*>& out);//sorts an array of actors by distance
-	void UpdateClosest();//Checks if there's a new closest or farthest actor and notify's them
+	void UpdateClosest();//Checks if there's a new closest or furthest actor and notifies them
 	void AllRays();//Sends a ray to every wanted actor
 	bool Ray(AActor* Target);//sends a ray to a specific actor to determine if it is visible
 	bool InRange(AActor* Target);//checks if the actor is not further than Detection Distance
 	void DrawDebugLines();//Draws connections to spotted wanted actors on screen
-	void DrawSingleLine(int i, FColor colllor);//Draws a line from this actor to a specyfic wanted actor
+	void DrawSingleLine(int i, FColor colllor);//Draws a line from this actor to a specific wanted actor
 	
 	void PrintList();//Shows the list of actors on screen
 
-	TArray<AActor*> CaF;//Closest and Farthest
+	TArray<AActor*> CaF;//Closest and Furthest
 
 };
